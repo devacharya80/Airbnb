d81e42950ec46114a3b92c411774cd3392d0060c
devacharya80 <dhanudeva80@gmail.com>
2026-03-05 10:58:16 +0530

    mongoSchema setup

---
+++ models/listing.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    price: {
+      type: Number,
+    },
+    location: {
+      type: String,
+    },
+    country: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
