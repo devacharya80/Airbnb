823baa92c74d4ff6e8c54350f3ab7f6ed298988d
devacharya80 <dhanudeva80@gmail.com>
2026-03-22 19:45:13 +0530

    Review Schema and model define

---
+++ models/review.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  comment: {
+    type: String,
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    default: 3,
+    max: 5,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
