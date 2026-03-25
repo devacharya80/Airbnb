6ee56ee433be9e1f774685030f7c763866c1552b
devacharya80 <dhanudeva80@gmail.com>
2026-03-25 22:06:27 +0530

    loggin middleware

---
+++ middleware.js
@@ -0,0 +1,9 @@
+const isLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    req.flash("error", "You must be logged in to create listing");
+    return res.redirect("/listings");
+  }
+  next();
+};
+
+module.exports = isLoggedIn;
