c4a5db3c93c605c57290dfb5241b7268f7e7b4ba
devacharya80 <dhanudeva80@gmail.com>
2026-03-30 23:08:33 +0530

    user-controller

---
+++ controllers/user.js
@@ -0,0 +1,40 @@
+const User = require("../models/user");
+
+module.exports.renderSignup = (req, res) => {
+  res.render("user/signup");
+};
+
+module.exports.createSignup = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const newUser = new User({ username, email });
+    const registeredUser = await User.register(newUser, password);
+
+    req.login(registeredUser, (err) => {
+      if (err) return next(err);
+      req.flash("success", "Welcome to Wanderlust");
+      res.redirect("/listings");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/signup");
+  }
+};
+
+module.exports.renderLogin = (req, res) => {
+  res.render("user/login");
+};
+
+module.exports.createLogin = (req, res) => {
+  req.flash("success", "Welcome Back!");
+  const redirectUrl = res.locals.redirectUrl || "/listings";
+  res.redirect(redirectUrl);
+};
+
+module.exports.logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+    req.flash("success", "See You Again!");
+    res.redirect("/listings");
+  });
+};
--- routes/user.js
+++ routes/user.js
@@ -5,36 +5,16 @@ const User = require("../models/user");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 
+const userControler = require("../controllers/user.js");
+
 // signup route
-router.get("/signup", (req, res) => {
-  res.render("user/signup");
-});
+router.get("/signup", userControler.renderSignup);
 
 // signup post
-router.post(
-  "/signup",
-  wrapAsync(async (req, res, next) => {
-    try {
-      const { username, email, password } = req.body;
-      const newUser = new User({ username, email });
-      const registeredUser = await User.register(newUser, password);
-
-      req.login(registeredUser, (err) => {
-        if (err) return next(err);
-        req.flash("success", "Welcome to Wanderlust");
-        res.redirect("/listings");
-      });
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("/signup");
-    }
-  }),
-);
+router.post("/signup", wrapAsync(userControler.createSignup));
 
 // login route
-router.get("/login", (req, res) => {
-  res.render("user/login");
-});
+router.get("/login", userControler.renderLogin);
 
 // login post
 router.post(
@@ -44,20 +24,10 @@ router.post(
     failureRedirect: "/login",
     failureFlash: true,
   }),
-  (req, res) => {
-    req.flash("success", "Welcome Back!");
-    const redirectUrl = res.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl);
-  },
+  userControler.createLogin,
 );
 
 // logout
-router.get("/logout", (req, res, next) => {
-  req.logout((err) => {
-    if (err) return next(err);
-    req.flash("success", "See You Again!");
-    res.redirect("/listings");
-  });
-});
+router.get("/logout", userControler.logout);
 
 module.exports = router;
